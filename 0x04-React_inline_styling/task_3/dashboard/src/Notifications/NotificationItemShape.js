bf0121c44a44ce3964808e507a5c203fa97e9520
Fatimazohra95 <fatimazohralakhal32@gmail.com>
2024-03-19 15:02:48 +0000

    Task

--- 0x04-React_inline_styling/task_3/dashboard/src/Notifications/NotificationItemShape.js
+++ 0x04-React_inline_styling/task_3/dashboard/src/Notifications/NotificationItemShape.js
@@ -1,8 +1,23 @@
-import PropType from 'prop-types';
+import React from 'react';
+import { shallow } from 'enzyme';
+import NotificationItem from './NotificationItem';
+import { StyleSheetTestUtils } from 'aphrodite';
 
-export default PropType.shape({
-  id: PropType.number.isRequired,
-  html: PropType.shape({__html: PropType.string}),
-  type: PropType.string.isRequired,
-  value: PropType.string
+describe("Testing <NotificationItem />", () => {
+  let  wrapper;
+  
+  beforeEach(() => {
+    StyleSheetTestUtils.suppressStyleInjection();
+  });
+
+  it("<NotificationItem /> renders without crashing", () => {
+    wrapper = shallow(<NotificationItem />);
+    expect(wrapper.exists());
+  });
+
+  it("<NotificationItem />  renders the correct html by passing dummy type and value props,", () => {
+    wrapper = shallow(<NotificationItem type="default" value="test" />);
+    expect(wrapper.find("li").text()).toBe("test");
+    expect(wrapper.find("li").prop("data-notification-type")).toBe("default");
+  });
 });
